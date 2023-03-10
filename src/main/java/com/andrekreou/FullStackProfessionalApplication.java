231c4a02c8198e445c09348b5210eb7309e83aac
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2023-03-10 18:34:04 +0200

    Initial commit

---
+++ src/main/java/com/andrekreou/FullStackProfessionalApplication.java
@@ -0,0 +1,13 @@
+package com.andrekreou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProfessionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackProfessionalApplication.class, args);
+	}
+
+}
