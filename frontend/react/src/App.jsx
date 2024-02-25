65875c14f13ad5727378dd4beef07916b802ee43
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2024-02-25 17:09:45 +0200

    Map Transformation

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,21 +1,45 @@
 import UserProfile from "./UserProfile.jsx";
 
+const users = [
+    {
+        name: "Jamilla",
+        age: 22,
+        gender: "FEMALE"
+    },
+    {
+        name: "Anna",
+        age: 45,
+        gender: "FEMALE"
+    },
+    {
+        name: "Alex",
+        age: 18,
+        gender: "MALE"
+    },
+    {
+        name: "Bilal",
+        age: 27,
+        gender: "MALE"
+    },
+]
+
+const UserProfiles = ({ users }) => (
+    <div>
+        {users.map((user, index) => (
+            <UserProfile
+                key={index}
+                name={user.name}
+                age={user.age}
+                gender={user.gender}
+                imageNumber={index}
+            />
+        ))}
+    </div>
+)
+
 function App() {
     return (
-        <div>
-            <UserProfile
-                name={"Jamila"}
-                age={22}
-                gender={"women"}>
-                <p>Hello</p>
-            </UserProfile>
-            <UserProfile
-                name={"Marco"}
-                age={34}
-                gender={"men"}>
-                <h1>Ciao</h1>
-            </UserProfile>
-        </div>
+        <UserProfiles users={users}/>
     )
 }
 
