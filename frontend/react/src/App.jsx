83e84c26e93307458c493a8fa384f6f11c9cb468
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2024-02-25 15:28:49 +0200

    Starting working with JSX and Props

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,5 +1,20 @@
+import UserProfile from "./UserProfile.jsx";
+
 function App() {
-    return <h1>Hello React</h1>
+    return (
+        <div>
+            <UserProfile
+                name={"Jamila"}
+                age={22}
+                gender={"women"}
+            />
+            <UserProfile
+                name={"Marco"}
+                age={34}
+                gender={"men"}
+            />
+        </div>
+    )
 }
 
 export default App
---
+++ frontend/react/src/UserProfile.jsx
@@ -0,0 +1,11 @@
+const UserProfile = ({name, age, gender}) => {
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p>{age}</p>
+            <img src={`https://randomuser.me/api/portraits/${gender}/75.jpg`}/>
+        </div>
+    )
+}
+
+export default UserProfile;
